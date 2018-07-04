3fde9af69f2055fc8995f5077f266bfc0a96ab07
ki <sk.kwon@battleent.com>
2018-07-04 20:03:40 +0900

    router 계정 등록 확인 코드 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import source from '../../build/contracts/Account.json'
 import Toons from '@/components/Toons'
 import Auth from '@/components/Auth'
 
 Vue.use(Router)
 
 const requireAuth = async (to, from, next) => {
+  const account = await web3.eth.getAccounts();
+  const contract = new web3.eth.Contract(source.abi, source.networks['3'].address);
+  const result = await contract.methods.getUserAddress(account[0]).call();
+  if(result == 0) {
+    console.log('error')
+  }
   return next();
 }
 
