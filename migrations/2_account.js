74721827a429335bc732432b77fab156c800e605
ki <sk.kwon@battleent.com>
2018-07-04 19:48:51 +0900

    deploy contract 환경 구성

---
+++ migrations/2_account.js
@@ -0,0 +1,5 @@
+var Contract = artifacts.require("Account");
+
+module.exports = function(deployer) {
+  deployer.deploy(Contract);
+};
--- truffle.js
+++ truffle.js
@@ -1,18 +1,19 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+require('dotenv-flow').config({default_node_env: 'ropsten'});
+var HDWalletProvider = require("truffle-hdwallet-provider-privkey");
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(process.env.PRIVATE_KEY, "https://ropsten.infura.io/"),
+      network_id: 3,
+      gas: 3000000
+    }
+  }
 };
