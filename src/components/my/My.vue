f1b80aefd1cd0756c5e56b37d3f3266814cc60d9
ki <sk.kwon@battleent.com>
2018-07-10 20:52:39 +0900

    회원 가입 제거

---
+++ src/components/my/My.vue
@@ -0,0 +1,66 @@
+<template>
+  <div role="group">
+    <label for="inputLive">Name:</label>
+    <b-form-input id="inputLive"
+                  v-model.trim="name"
+                  type="text"
+                  :disabled="registered"
+                  :state="nameState"
+                  aria-describedby="inputLiveHelp inputLiveFeedback"
+                  placeholder="Enter your name"></b-form-input>
+    <b-form-invalid-feedback id="inputLiveFeedback">
+      <!-- This will only be shown if the preceeding input has an invalid state -->
+      Enter at least 3 letters
+    </b-form-invalid-feedback>
+    <b-form-text id="inputLiveHelp">
+      <!-- this is a form text block (formerly known as help block) -->
+      Your full name.
+    </b-form-text>
+    <b-button :disabled="!nameState" v-if="!registered" variant="success" @click="submit()">Submit</b-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'My',
+    computed: {
+      nameState() {
+        return this.name && this.name.length > 2 ? true : false
+      }
+    },
+    data() {
+      return {
+        registered: true,
+        name: null,
+      }
+    },
+    methods: {
+      async submit() {
+        const userName = await this.$contract.account.getUserName(this.$root.account);
+        const address = await this.$contract.account.getUserAddress(this.name);
+        if (userName) {
+          this.$toast.center('이미 등록된 주소입니다');
+        } else if (address > 0) {
+          this.$toast.center('이미 등록된 사용자명입니다');
+        } else {
+          this.$loading('loading...');
+          try {
+            await this.$contract.account.createAccount(this.name);
+            window.location.reload()
+          } catch (e) {
+            alert(e)
+          }
+          this.$loading.close();
+        }
+      },
+    },
+    async created() {
+      this.name = await this.$contract.account.getUserName(this.$root.account);
+      this.registered = (this.name != null && this.name.length > 0);
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Join from '@/components/Join'
 import Comics from '@/components/comics/Comics'
 import PostComics from '@/components/post-comics/PostComics'
 import Episodes from '@/components/comics/episodes/Episodes'
 import Viewer from '@/components/comics/episodes/viewer/Viewer'
-import My from '@/components/My'
+import My from '@/components/my/My'
 
 Vue.use(Router)
-
-const requireAuth = async (to, from, next) => {
-  const app = await router.app;
-  const result = await app.$contract.account.getUserName(app.account);
-  if (!result) {
-    return next({path: '/join'})
-  } else {
-    return next();
-  }
-}
 
 const router = new Router({
   mode: 'history',
   routes: [
-    {path: '/join', name: 'join', component: Join,},
     {path: '/', redirect: '/comics'},
     {path: '/comics', name: 'comics', component: Comics},
     {path: '/comics/posts', name: 'post-comics', component: PostComics},
     {path: '/comics/:comic_id', name: 'episodes', component: Episodes, props: true},
     {path: '/comics/:comic_id/episodes/:episode_id', name: 'viewer', component: Viewer, props: true},
-    {path: '/my', name: 'my', component: My, beforeEnter: requireAuth}
+    {path: '/my', name: 'my', component: My}
   ],
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
