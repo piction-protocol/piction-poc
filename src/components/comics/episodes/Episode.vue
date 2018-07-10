a1ed745840881b639a82d8c9ea1400564899ea11
ki <sk.kwon@battleent.com>
2018-07-10 18:53:37 +0900

    에피소드 목록 UI 수정

--- src/components/comics/episodes/Episode.vue
+++ src/components/comics/episodes/Episode.vue
@@ -1,9 +1,16 @@
 <template>
   <router-link :to="{ name: 'viewer', params: { comic_id: comic_id, episode_id:episode.id }}" class="episode">
+      <b-row class="text-left" align-v="center">
+        <b-col cols="3"><img :src="episode.thumbnail"/></b-col>
+        <b-col>{{episode.title}}</b-col>
+      </b-row>
+  </router-link>
+  <!--
+  <router-link :to="{ name: 'viewer', params: { comic_id: comic_id, episode_id:episode.id }}" class="episode">
     <b-card :title="episode.title"
             :img-src="episode.thumbnail"
             img-alt="Image"
-            img-top
+            img-left
             tag="article"
             style="max-width: 20rem;"
             class="mb-2">
@@ -12,6 +19,7 @@
       </p>
     </b-card>
   </router-link>
+  -->
 </template>
 
 <script>
@@ -26,8 +34,12 @@
 </script>
 
 <style scoped>
+  img {
+    width: 100%;
+  }
+
   .episode {
-    display: inline-block;
-    margin: 7px;
+    display: block;
+    padding: 2px;
   }
 </style>
