cb89283b34af3fabebc33f0b93c1bf97579aeb8a
ki <sk.kwon@battleent.com>
2018-07-13 16:43:30 +0900

    이미지 해상도 정보 조회 기능 추가

--- src/plugins/piction-network-plugin.js
+++ src/plugins/piction-network-plugin.js
@@ -1,5 +1,7 @@
 import Account from './abi-class/Account.js'
+import Content from './abi-class/Content.js'
 import accountSource from '../../build/contracts/Account.json'
+import contentSource from '../../build/contracts/Content.json'
 
 const PictionNetworkPlugin = {
   install(Vue, options) {
@@ -11,6 +13,25 @@ const PictionNetworkPlugin = {
       options.account
     )
     Vue.prototype.$contract.account = account
+
+    const content = new Content(
+      contentSource.abi,
+      options.account,
+      contentSource.bytecode
+    )
+    Vue.prototype.$contract.content = content
+
+    Vue.prototype.$utils = {
+      getImageDimensions(dataUri) {
+        return new Promise((resolved, rejected) => {
+          const i = new Image();
+          i.onload = () => {
+            resolved({w: i.width, h: i.height})
+          };
+          i.src = dataUri
+        })
+      }
+    }
   }
 }
 
