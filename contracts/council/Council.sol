7c17fcafafdacac85966f14f589c18c1ba8d2cc8
jhSeo <jh.seo@battleent.com>
2018-07-13 16:04:30 +0900

    Council에 RoleManager, ContentManager 추가

--- contracts/council/Council.sol
+++ contracts/council/Council.sol
@@ -12,9 +12,17 @@ contract Council is ExtendsOwnable {
     uint256 public translatorRate;
     uint256 public deposit;
     address public token;
+    address public roleManager;
+    address public contentManager;
 
     modifier validRange(uint256 _value) {
         require(_value > 0);
+        _;
+    }
+
+    modifier validAddress(address _account) {
+        require(_account != address(0));
+        require(_account != address(this));
         _;
     }
 
@@ -53,6 +61,19 @@ contract Council is ExtendsOwnable {
         emit ChangeDistributionRate(msg.sender, "deposit");
     }
 
-    event RegisterCouncil(address _addr, uint256 _cpRate, uint256 _translatorRate, uint256 _deposit, address _token);
-    event ChangeDistributionRate(address _addr, string _name);
+    function setRoleManager(address _roleManager) external onlyOwner validAddress(_roleManager) {
+        roleManager = _roleManager;
+
+        emit ChangeAddress(msg.sender, "role manager", _roleManager);
+    }
+
+    function setContentManager(address _contentManager) external onlyOwner validAddress(_contentManager) {
+        contentManager = _contentManager;
+
+        emit ChangeAddress(msg.sender, "content manager", _contentManager);
+    }
+
+    event RegisterCouncil(address _sender, uint256 _cpRate, uint256 _translatorRate, uint256 _deposit, address _token);
+    event ChangeDistributionRate(address _sender, string _name);
+    event ChangeAddress(address _sender, string addressName, address _addr);
 }
