ba1dd5f026d711204cfeacb55c2a2c2b763001a0
jhSeo <jh.seo@battleent.com>
2018-07-13 11:22:10 +0900

    Marketer Contract 추가

---
+++ contracts/marketer/Marketer.sol
@@ -0,0 +1,31 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @title Marketer contract
+ *
+ * @author Junghoon Seo - <jh.seo@battleent.com>
+ */
+contract Marketer {
+  mapping (bytes32 => address) marketerInfo;
+
+  modifier validAddress(address _account) {
+      require(_account != address(0));
+      require(_account != address(this));
+      _;
+  }
+
+  constructor () public {
+
+  }
+
+  function getMarketerKey() public validAddress(msg.sender) returns(bytes32) {
+      bytes32 key = bytes32(keccak256(abi.encodePacked(msg.sender)));
+      marketerInfo[key] = msg.sender;
+
+      return key;
+  }
+
+  function getMarketerAddress(bytes32 _key) public validAddress(msg.sender) view returns(address) {
+      return marketerInfo[_key];
+  }
+}
