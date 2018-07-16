7b21d83407a06eae195d4df678ff93575529adc3
jhSeo <jh.seo@battleent.com>
2018-07-16 10:43:51 +0900

    Council translatorRate 제거

--- contracts/council/Council.sol
+++ contracts/council/Council.sol
@@ -9,7 +9,6 @@ import "contracts/utils/ExtendsOwnable.sol";
  */
 contract Council is ExtendsOwnable {
     uint256 public cdRate;
-    uint256 public translatorRate;
     uint256 public deposit;
     address public token;
     address public roleManager;
@@ -28,31 +27,23 @@ contract Council is ExtendsOwnable {
 
     constructor(
         uint256 _cdRate,
-        uint256 _translatorRate,
         uint256 _deposit,
         address _token
     ) public {
-        require(_cdRate > 0 && _translatorRate > 0 && _deposit > 0);
+        require(_cdRate > 0 && _deposit > 0);
         require(_token != address(0) && _token != address(this));
 
         cdRate = _cdRate;
-        translatorRate = _translatorRate;
         deposit = _deposit;
         token = _token;
 
-        emit RegisterCouncil(msg.sender, _cdRate, _translatorRate, _deposit, _token);
+        emit RegisterCouncil(msg.sender, _cdRate, _deposit, _token);
     }
 
     function setCdRate(uint256 _cdRate) external onlyOwner validRange(_cdRate) {
         cdRate = _cdRate;
 
         emit ChangeDistributionRate(msg.sender, "cd rate");
-    }
-
-    function setTranslatorRate(uint256 _translatorRate) external onlyOwner validRange(_translatorRate) {
-        translatorRate = _translatorRate;
-
-        emit ChangeDistributionRate(msg.sender, "translator rate");
     }
 
     function setDeposit(uint256 _deposit) external onlyOwner validRange(_deposit) {
@@ -73,7 +64,7 @@ contract Council is ExtendsOwnable {
         emit ChangeAddress(msg.sender, "content manager", _contentManager);
     }
 
-    event RegisterCouncil(address _sender, uint256 _cdRate, uint256 _translatorRate, uint256 _deposit, address _token);
+    event RegisterCouncil(address _sender, uint256 _cdRate, uint256 _deposit, address _token);
     event ChangeDistributionRate(address _sender, string _name);
     event ChangeAddress(address _sender, string addressName, address _addr);
 }
