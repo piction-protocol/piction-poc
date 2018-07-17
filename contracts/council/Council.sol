9fe3bed0c1a390b1f94920ef9e4d636b8cf60e1e
jhSeo <jh.seo@battleent.com>
2018-07-17 17:31:29 +0900

    Council reportRegistrationFee 추가

--- contracts/council/Council.sol
+++ contracts/council/Council.sol
@@ -12,6 +12,7 @@ contract Council is ExtendsOwnable {
     uint256 public depositRate;
     uint256 public initialDeposit;
     uint256 public userPaybackRate;
+    uint256 public reportRegistrationFee;
     address public userPaybackPool;
     address public token;
     address public roleManager;
@@ -33,21 +34,24 @@ contract Council is ExtendsOwnable {
         uint256 _depositRate,
         uint256 _initialDeposit,
         uint256 _userPaybackRate,
+        uint256 _reportRegistrationFee,
         address _token)
         public
         validRange(_cdRate)
         validRange(_depositRate)
         validRange(_initialDeposit)
         validRange(_userPaybackRate)
+        validRange(_reportRegistrationFee)
         validAddress(_token)
     {
         cdRate = _cdRate;
         depositRate = _depositRate;
         initialDeposit = _initialDeposit;
         userPaybackRate = _userPaybackRate;
+        reportRegistrationFee = _reportRegistrationFee;
         token = _token;
 
-        emit RegisterCouncil(msg.sender, _cdRate, _depositRate, _initialDeposit, _userPaybackRate, _token);
+        emit RegisterCouncil(msg.sender, _cdRate, _depositRate, _initialDeposit, _userPaybackRate, _reportRegistrationFee, _token);
     }
 
     function setCdRate(uint256 _cdRate) external onlyOwner validRange(_cdRate) {
@@ -74,6 +78,12 @@ contract Council is ExtendsOwnable {
         emit ChangeDistributionRate(msg.sender, "user payback rate", _userPaybackRate);
     }
 
+    function setReportRegistrationFee(uint256 _reportRegistrationFee) external onlyOwner validRange(_reportRegistrationFee) {
+        reportRegistrationFee = _reportRegistrationFee;
+
+        emit ChangeDistributionRate(msg.sender, "report registration fee", _reportRegistrationFee);
+    }
+
     function setUserPaybackPool(address _userPaybackPool) external onlyOwner validAddress(_userPaybackPool) {
         userPaybackPool = _userPaybackPool;
 
@@ -92,7 +102,7 @@ contract Council is ExtendsOwnable {
         emit ChangeAddress(msg.sender, "contents manager", _contentsManager);
     }
 
-    event RegisterCouncil(address _sender, uint256 _cdRate, uint256 _deposit, uint256 _initialDeposit, uint256 _userPaybackRate, address _token);
+    event RegisterCouncil(address _sender, uint256 _cdRate, uint256 _deposit, uint256 _initialDeposit, uint256 _userPaybackRate, uint256 _reportRegistrationFee, address _token);
     event ChangeDistributionRate(address _sender, string _name, uint256 _value);
     event ChangeAddress(address _sender, string addressName, address _addr);
 }
