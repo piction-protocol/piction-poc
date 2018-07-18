cce486e57eadca5a3e04797933c9e3db488d8539
jhSeo <jh.seo@battleent.com>
2018-07-18 19:07:58 +0900

    Marketer key 형 변환 bytes32 -> bytes

--- contracts/marketer/Marketer.sol
+++ contracts/marketer/Marketer.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.4.24;
  * @author Junghoon Seo - <jh.seo@battleent.com>
  */
 contract Marketer {
-  mapping (bytes32 => address) marketerInfo;
+  mapping (bytes => address) marketerInfo;
 
   modifier validAddress(address _account) {
       require(_account != address(0));
@@ -14,14 +14,14 @@ contract Marketer {
       _;
   }
 
-  function getMarketerKey() public validAddress(msg.sender) returns(bytes32) {
-      bytes32 key = bytes32(keccak256(abi.encodePacked(msg.sender)));
+  function getMarketerKey() public validAddress(msg.sender) returns(bytes) {
+      bytes key = bytes(keccak256(abi.encodePacked(msg.sender)));
       marketerInfo[key] = msg.sender;
 
       return key;
   }
 
-  function getMarketerAddress(bytes32 _key) public validAddress(msg.sender) view returns(address) {
+  function getMarketerAddress(bytes _key) public validAddress(msg.sender) view returns(address) {
       return marketerInfo[_key];
   }
 }
