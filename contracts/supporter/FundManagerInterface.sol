6293f4c884ab2b6ac687efad88426929e5c82c65
ki <sk.kwon@battleent.com>
2018-07-27 16:34:54 +0900

    FundManagerInterface 구현

---
+++ contracts/supporter/FundManagerInterface.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+contract FundManagerInterface {
+
+    function getFunds(address _content) public returns (address[]);
+
+    function getDistributeAmount(address _fund, uint256 _total) public view returns (address[], uint256[]);
+
+    event RegisterFund(address _content, address _fund);
+}
