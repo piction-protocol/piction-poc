74c9dbb331da9a692b217c0ea2538b6747e51bbb
ki <sk.kwon@battleent.com>
2018-08-01 12:27:59 +0900

    PXL 배포한 후에 언락 처리

--- migrations/2_pxl.js
+++ migrations/2_pxl.js
@@ -1,5 +1,7 @@
 var Contract = artifacts.require("PXL");
 
 module.exports = function (deployer) {
-    deployer.deploy(Contract, 10000000 * Math.pow(10, 18));
+  deployer.deploy(Contract, 10000000 * Math.pow(10, 18)).then(function (instance) {
+        instance.unlock();
+  });
 };
