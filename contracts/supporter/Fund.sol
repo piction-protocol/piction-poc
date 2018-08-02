b29f41836311fbeb0f683f3f799fe126f1d4e27c
sk.kwon <sk.kwon@battleent.com>
2018-08-02 19:45:16 +0900

    Fund 정보 조회 기능 추가

--- contracts/supporter/Fund.sol
+++ contracts/supporter/Fund.sol
@@ -12,169 +12,170 @@ import "openzeppelin-solidity/contracts/math/Math.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Fund is ContractReceiver, ExtendsOwnable, ValidValue {
-    using Math for uint256;
-    using SafeMath for uint256;
-    using SafeERC20 for ERC20;
-    using TimeLib for *;
+	using Math for uint256;
+	using SafeMath for uint256;
+	using SafeERC20 for ERC20;
+	using TimeLib for *;
 
-    struct Supporter {
-        address user;
-        uint256 investment;
-        uint256 collection;
-        uint256 distributionRate;
-    }
+	struct Supporter {
+		address user;
+		uint256 investment;
+		uint256 collection;
+		uint256 distributionRate;
+	}
 
-    uint256 startTime;
-    uint256 endTime;
-    string detail;
+	uint256 startTime;
+	uint256 endTime;
+	string detail;
 
-    address council;
-    address content;
-    address writer;
+	address council;
+	address content;
+	address writer;
 
-    uint256 fundRise;
-    Supporter[] supporters;
-    uint256 poolSize;
-    uint256 releaseInterval;
-    uint256 distributionRate;
+	uint256 fundRise;
+	Supporter[] supporters;
+	uint256 poolSize;
+	uint256 releaseInterval;
+	uint256 distributionRate;
 
-    SponsorshipPool public sponsorshipPool;
+	SponsorshipPool public sponsorshipPool;
 
-    constructor(
-        address _council,
-        address _content,
-        address _writer,
-        uint256 _startTime,
-        uint256 _endTime,
-        uint256 _poolSize,
-        uint256 _releaseInterval,
-        uint256 _distributionRate,
-        string _detail)
-    public validAddress(_content) validAddress(_writer) validAddress(_council) {
-        require(_startTime > TimeLib.currentTime());
-        require(_endTime > _startTime);
-        require(_poolSize > 0);
-        require(_releaseInterval > 0);
-        require(distributionRate <= 100);
+	constructor(
+		address _council,
+		address _content,
+		address _writer,
+		uint256 _startTime,
+		uint256 _endTime,
+		uint256 _poolSize,
+		uint256 _releaseInterval,
+		uint256 _distributionRate,
+		string _detail)
+	public validAddress(_content) validAddress(_writer) validAddress(_council) {
+		require(_startTime > TimeLib.currentTime());
+		require(_endTime > _startTime);
+		require(_poolSize > 0);
+		require(_releaseInterval > 0);
+		require(distributionRate <= 100);
 
-        council = _council;
-        startTime = _startTime;
-        endTime = _endTime;
-        detail = _detail;
-        poolSize = _poolSize;
-        releaseInterval = _releaseInterval;
-        distributionRate = _distributionRate;
-    }
+		council = _council;
+		startTime = _startTime;
+		endTime = _endTime;
+		detail = _detail;
+		poolSize = _poolSize;
+		releaseInterval = _releaseInterval;
+		distributionRate = _distributionRate;
+	}
 
-    function receiveApproval(address _from, uint256 _value, address _token, string _data) public validAddress(_from) validAddress(_token) {
-        support(_from, _value, _token);
-    }
+	function receiveApproval(address _from, uint256 _value, address _token, string _data) public validAddress(_from) validAddress(_token) {
+		support(_from, _value, _token);
+	}
 
-    function support(address _from, uint256 _value, address _token) private {
-        require(TimeLib.currentTime().between(startTime, endTime));
+	function support(address _from, uint256 _value, address _token) private {
+		require(TimeLib.currentTime().between(startTime, endTime));
 
-        ERC20 token = ERC20(CouncilInterface(council).getToken());
-        require(address(token) == _token);
+		ERC20 token = ERC20(CouncilInterface(council).getToken());
+		require(address(token) == _token);
 
-        (uint256 index, bool success) = findSupporterIndex(_from);
-        if (success) {
-            supporters[index].investment = supporters[index].investment.add(_value);
-        } else {
-            supporters.push(Supporter(_from, _value, 0, 0));
-        }
-        fundRise = fundRise.add(_value);
-        token.safeTransferFrom(_from, address(this), _value);
+		(uint256 index, bool success) = findSupporterIndex(_from);
+		if (success) {
+			supporters[index].investment = supporters[index].investment.add(_value);
+		} else {
+			supporters.push(Supporter(_from, _value, 0, 0));
+		}
+		fundRise = fundRise.add(_value);
+		token.safeTransferFrom(_from, address(this), _value);
 
-        emit Support(_from, _value);
-    }
+		emit Support(_from, _value);
+	}
 
-    function startDistribution() external {
-        require(address(sponsorshipPool) == address(0));
-        require(TimeLib.currentTime() < endTime);
-        require(fundRise > 0);
+	function startDistribution() external {
+		require(address(sponsorshipPool) == address(0));
+		require(TimeLib.currentTime() < endTime);
+		require(fundRise > 0);
 
-        setDistributionRate();
-        sponsorshipPool = new SponsorshipPool(fundRise, poolSize, releaseInterval);
-    }
+		setDistributionRate();
+		sponsorshipPool = new SponsorshipPool(fundRise, poolSize, releaseInterval);
+	}
 
-    function setDistributionRate() private {
-        uint256 totalInvestment;
-        for (uint256 i = 0; i < supporters.length; i++) {
-            totalInvestment = totalInvestment.add(supporters[i].investment);
-        }
-        for (i = 0; i < supporters.length; i++) {
-            supporters[i].distributionRate = totalInvestment.div(supporters[i].investment);
-        }
-    }
+	function setDistributionRate() private {
+		uint256 totalInvestment;
+		for (uint256 i = 0; i < supporters.length; i++) {
+			totalInvestment = totalInvestment.add(supporters[i].investment);
+		}
+		for (i = 0; i < supporters.length; i++) {
+			supporters[i].distributionRate = totalInvestment.div(supporters[i].investment);
+		}
+	}
 
-    function releasePool() external validAddress(sponsorshipPool) {
-        uint256 amount = sponsorshipPool.release();
-        ERC20 token = ERC20(CouncilInterface(council).getToken());
-        token.safeTransfer(writer, amount);
-    }
+	function releasePool() external validAddress(sponsorshipPool) {
+		uint256 amount = sponsorshipPool.release();
+		ERC20 token = ERC20(CouncilInterface(council).getToken());
+		token.safeTransfer(writer, amount);
+	}
 
-    function vote() external validAddress(sponsorshipPool) {
-        require(isSupporter(msg.sender));
+	function vote() external validAddress(sponsorshipPool) {
+		require(isSupporter(msg.sender));
 
-        sponsorshipPool.vote(msg.sender);
-        (address[] memory _supporters,) = getSupporters();
-        uint256 votingCount = sponsorshipPool.getVotingCount(_supporters);
-        if (supporters.length.div(2) <= votingCount) {
-            uint256 cancelAmount = sponsorshipPool.cancelPool();
-            sponsorshipPool.addPool(cancelAmount);
-        }
-    }
+		sponsorshipPool.vote(msg.sender);
+		(address[] memory _supporters,) = getSupporters();
+		uint256 votingCount = sponsorshipPool.getVotingCount(_supporters);
+		if (supporters.length.div(2) <= votingCount) {
+			uint256 cancelAmount = sponsorshipPool.cancelPool();
+			sponsorshipPool.addPool(cancelAmount);
+		}
+	}
 
-    function distribution(uint256 _total) external returns (address[], uint256[]) {
-        address[] memory _supporters = new address[](supporters.length);
-        uint256[] memory _amounts = new uint256[](supporters.length);
+	function distribution(uint256 _total) external returns (address[], uint256[]) {
+		address[] memory _supporters = new address[](supporters.length);
+		uint256[] memory _amounts = new uint256[](supporters.length);
 
-        for (uint256 i = 0; i < supporters.length; i++) {
-            _supporters[i] = supporters[i].user;
-            uint256 remain = supporters[i].investment.sub(supporters[i].collection);
-            if (remain == 0) {
-                _amounts[i] = _total.div(distributionRate).div(supporters[i].distributionRate);
-            } else {
-                _amounts[i] = _total.div(supporters[i].distributionRate).min256(remain);
-                supporters[i].collection = supporters[i].collection.add(_amounts[i]);
-            }
-        }
-        return (_supporters, _amounts);
-    }
+		for (uint256 i = 0; i < supporters.length; i++) {
+			_supporters[i] = supporters[i].user;
+			uint256 remain = supporters[i].investment.sub(supporters[i].collection);
+			if (remain == 0) {
+				_amounts[i] = _total.div(distributionRate).div(supporters[i].distributionRate);
+			} else {
+				_amounts[i] = _total.div(supporters[i].distributionRate).min256(remain);
+				supporters[i].collection = supporters[i].collection.add(_amounts[i]);
+			}
+		}
+		return (_supporters, _amounts);
+	}
 
-    function getSupporters() public view returns (address[], uint256[]) {
-        address[] memory user = new address[](supporters.length - 1);
-        uint256[] memory investment = new uint256[](supporters.length - 1);
+	function getSupporters() public view returns (address[], uint256[]) {
+		address[] memory user = new address[](supporters.length - 1);
+		uint256[] memory investment = new uint256[](supporters.length - 1);
 
-        uint256 supportersIndex = 0;
-        for (uint256 i = 0; i < supporters.length; i++) {
-            user[supportersIndex] = supporters[i].user;
-            investment[supportersIndex] = supporters[i].investment;
+		uint256 supportersIndex = 0;
+		for (uint256 i = 0; i < supporters.length; i++) {
+			user[supportersIndex] = supporters[i].user;
+			investment[supportersIndex] = supporters[i].investment;
 
-            supportersIndex = supportersIndex.add(1);
-        }
-        return (user, investment);
-    }
+			supportersIndex = supportersIndex.add(1);
+		}
+		return (user, investment);
+	}
 
-    function getDistributionRate() public view returns (uint256){
-        return distributionRate;
-    }
+	function info() external view returns (uint256, uint256, uint256, uint256, uint256, uint256, string){
+		return (startTime, endTime, fundRise, poolSize, releaseInterval, distributionRate, detail);
+	}
 
-    function findSupporterIndex(address _supporter) private view returns (uint, bool){
-        for (uint256 i = 0; i < supporters.length; i++) {
-            if (supporters[i].user == _supporter) {
-                return (i, true);
-            }
-        }
-    }
 
-    function isSupporter(address _supporter) private view returns (bool){
-        for (uint256 i = 0; i < supporters.length; i++) {
-            if (supporters[i].user == _supporter) {
-                return true;
-            }
-        }
-    }
+	function findSupporterIndex(address _supporter) private view returns (uint, bool){
+		for (uint256 i = 0; i < supporters.length; i++) {
+			if (supporters[i].user == _supporter) {
+				return (i, true);
+			}
+		}
+	}
 
-    event Support(address _from, uint256 _amount);
+	function isSupporter(address _supporter) private view returns (bool){
+		for (uint256 i = 0; i < supporters.length; i++) {
+			if (supporters[i].user == _supporter) {
+				return true;
+			}
+		}
+	}
+
+	event Support(address _from, uint256 _amount);
 }
