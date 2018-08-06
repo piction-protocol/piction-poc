4d11cb40eb08ccad1442720b6ffc724fd3e1c91b
jhSeo <jh.seo@battleent.com>
2018-08-06 14:45:04 +0900

    Marketer Test Code 작성 및 Contract 수정 (key 생성과 mapping을 분리)

--- contracts/marketer/Marketer.sol
+++ contracts/marketer/Marketer.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.24;
 
+import "contracts/utils/ExtendsOwnable.sol";
 import "contracts/marketer/MarketerInterface.sol";
 import "contracts/utils/ValidValue.sol";
 
@@ -8,17 +9,19 @@ import "contracts/utils/ValidValue.sol";
  *
  * @author Junghoon Seo - <jh.seo@battleent.com>
  */
-contract Marketer is MarketerInterface, ValidValue {
-  mapping (bytes32 => address) marketerInfo;
+contract Marketer is ExtendsOwnable, MarketerInterface, ValidValue {
+    mapping (bytes32 => address) marketerInfo;
 
-  function getMarketerKey() public returns(bytes32) {
-      bytes32 key = bytes32(keccak256(abi.encodePacked(msg.sender)));
-      marketerInfo[key] = msg.sender;
+    function generateMarketerKey() public returns(bytes32) {
+        bytes32 key = bytes32(keccak256(abi.encodePacked(msg.sender)));
+        return key;
+    }
 
-      return key;
-  }
+    function setMarketerKey(address _marketer, bytes32 _key) external onlyOwner validAddress(_marketer) {
+        marketerInfo[_key] = _marketer;
+    }
 
-  function getMarketerAddress(bytes32 _key) public view returns(address) {
-      return marketerInfo[_key];
-  }
+    function getMarketerAddress(bytes32 _key) public view returns(address) {
+        return marketerInfo[_key];
+    }
 }
