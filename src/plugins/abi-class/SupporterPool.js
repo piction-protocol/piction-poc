0c4833e268630d322685dab14a5d4d350359d6a1
sk.kwon <sk.kwon@battleent.com>
2018-08-06 14:14:10 +0900

    web3 플러그인 SupporterPool 추가

--- src/plugins/abi-class/SupporterPool.js
+++ src/plugins/abi-class/SupporterPool.js
@@ -1,19 +1,19 @@
-class Fund {
+class SupporterPool {
   constructor(abi, from) {
     this._contract = new web3.eth.Contract(abi);
     this._contract.options.from = from;
     this._contract.options.gas = 6000000;
   }
 
-  getInfo(address) {
+  getDistributions(address) {
     this._contract.options.address = address;
-    return this._contract.methods.info().call()
+    return this._contract.methods.getDistributions().call()
   }
 
-  getSupporters(address) {
+  distribution(address) {
     this._contract.options.address = address;
-    return this._contract.methods.getSupporters().call()
+    return this._contract.methods.distribution().send()
   }
 }
 
-export default Fund;
+export default SupporterPool;
