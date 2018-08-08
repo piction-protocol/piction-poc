9cc98d518e0a7bf4b4ec47163ce465b6bd73fa39
sk.kwon <sk.kwon@battleent.com>
2018-08-08 15:34:46 +0900

    필요 없는 코드 제거

--- src/components/my/Index.vue
+++ src/components/my/Index.vue
@@ -11,40 +11,11 @@
 
 <script>
   export default {
-    computed: {
-      nameState() {
-        return this.name && this.name.length > 2 ? true : false
-      }
-    },
     data() {
-      return {
-        registered: true,
-        name: null,
-      }
+      return {}
     },
-    methods: {
-      async submit() {
-        const userName = await this.$contract.account.getUserName(this.$root.account);
-        const address = await this.$contract.account.getUserAddress(this.name);
-        if (userName) {
-          this.$toast.center('이미 등록된 주소입니다');
-        } else if (address > 0) {
-          this.$toast.center('이미 등록된 사용자명입니다');
-        } else {
-          this.$loading('loading...');
-          try {
-            await this.$contract.account.createAccount(this.name);
-            window.location.reload()
-          } catch (e) {
-            alert(e)
-          }
-          this.$loading.close();
-        }
-      },
-    },
-    async created() {
-      this.name = await this.$contract.account.getUserName(this.$root.account);
-      this.registered = (this.name != null && this.name.length > 0);
+    methods: {},
+    created() {
     }
   }
 </script>
