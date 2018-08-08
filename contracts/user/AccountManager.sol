4713237c7a49b8f911d5a83c37fd73ab44233526
sk.kwon <sk.kwon@battleent.com>
2018-08-08 17:46:53 +0900

    계정 생성시에 토큰 airdrop

--- contracts/user/AccountManager.sol
+++ contracts/user/AccountManager.sol
@@ -1,33 +1,48 @@
 pragma solidity ^0.4.24;
 
+import "contracts/council/CouncilInterface.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
+
 contract AccountManager {
+	using SafeERC20 for ERC20;
 
-    mapping (string => address) userNameToAddress;
-    mapping (address => string) addressToUserName;
+	mapping(string => address) userNameToAddress;
+	mapping(address => string) addressToUserName;
 
-    modifier validAddress(address _account) {
-        require(_account != address(0));
-        require(_account != address(this));
-        _;
-    }
+	modifier validAddress(address _account) {
+		require(_account != address(0));
+		require(_account != address(this));
+		_;
+	}
 
-    constructor () public {
+	address council;
+	uint256 airdropAmount;
 
-    }
+	constructor (address _council, uint256 _airdropAmount) public {
+		council = _council;
+		airdropAmount = _airdropAmount;
+	}
 
-    function createAccount(string _userName) public validAddress(msg.sender) {
-        require(userNameToAddress[_userName] == address(0));
-        require(bytes(addressToUserName[msg.sender]).length == 0);
+	function createAccount(string _userName) public validAddress(msg.sender) {
+		require(userNameToAddress[_userName] == address(0));
+		require(bytes(addressToUserName[msg.sender]).length == 0);
 
-        userNameToAddress[_userName] = msg.sender;
-        addressToUserName[msg.sender] = _userName;
-    }
+		userNameToAddress[_userName] = msg.sender;
+		addressToUserName[msg.sender] = _userName;
 
-    function getUserName(address _addr) public validAddress(_addr) view returns(string) {
-        return addressToUserName[_addr];
-    }
+		// POC airdrop
+		ERC20 token = ERC20(CouncilInterface(council).getToken());
+		if (airdropAmount > 0 && token.balanceOf(address(this)) >= airdropAmount) {
+			token.safeTransfer(msg.sender, airdropAmount);
+		}
+	}
 
-    function getUserAddress(string _userName) public view returns(address) {
-        return userNameToAddress[_userName];
-    }
+	function getUserName(address _addr) public validAddress(_addr) view returns (string) {
+		return addressToUserName[_addr];
+	}
+
+	function getUserAddress(string _userName) public view returns (address) {
+		return userNameToAddress[_userName];
+	}
 }
