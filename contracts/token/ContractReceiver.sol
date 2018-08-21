8f58f31bb4276db852d629faf1da9041ac0a829a
charls.kim <cs.kim@battleent.com>
2018-08-21 16:19:19 +0900

    approveAndCall 관련 인터페이스 파라미터 변경

--- contracts/token/ContractReceiver.sol
+++ contracts/token/ContractReceiver.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.24;
 
 contract ContractReceiver {
-    function receiveApproval(address _from, uint256 _value, address _token, address[] _address, uint256 _index) public;
+    function receiveApproval(address _from, uint256 _value, address _token, bytes _data) public;
 }
