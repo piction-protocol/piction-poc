dbd11a6e67ee4adced69da9637fe494c4277e7ab
sk.kwon <sk.kwon@battleent.com>
2018-09-03 16:05:05 +0900

    truffle private network 추가

--- truffle.js
+++ truffle.js
@@ -1,4 +1,4 @@
-require('dotenv-flow').config({default_node_env: 'ropsten'});
+require('dotenv-flow').config({default_node_env: 'private'});
 var HDWalletProvider = require("truffle-hdwallet-provider-privkey");
 
 module.exports = {
@@ -14,6 +14,12 @@ module.exports = {
       provider: new HDWalletProvider(process.env.PRIVATE_KEY, "https://ropsten.infura.io/"),
       network_id: 3,
       gas: 7000000,
+      gasPrice: 100000000000
+    },
+    private: {
+      provider: new HDWalletProvider(process.env.PRIVATE_KEY, "http://52.203.39.129:8545/"),
+      network_id: 2323,
+      gas: 6000000,
       gasPrice: 100000000000
     }
   }
