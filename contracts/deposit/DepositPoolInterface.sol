a5feffcf22f1f82c6af2c82feccaf87d2a816432
변동명 <dm.byun@thewhalegames.co.kr>
2018-09-05 19:02:14 +0900

    신고 처리 후 결과 값 추가 저장

--- contracts/deposit/DepositPoolInterface.sol
+++ contracts/deposit/DepositPoolInterface.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.24;
 
 interface DepositPoolInterface {
-    function reportReward(address _content, address _reporter) external;
+    function reportReward(address _content, address _reporter) external returns(uint256);
 }
--- contracts/report/ReportInterface.sol
+++ contracts/report/ReportInterface.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.24;
 
 interface ReportInterface {
-    function completeReport(uint256 _index) external;
-    function deduction(address _reporter, uint256 _rate, bool _block) external;
+    function completeReport(uint256 _index, bool _valid, uint256 _resultAmount) external;
+    function deduction(address _reporter, uint256 _rate, bool _block) external returns(uint256);
     function getReportCount(address _content) external view returns(uint256);
     function getUncompletedReport(address _content) external view returns(uint256 count);
 }
