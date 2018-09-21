fbc3c2814442b7633dc1314e1db74a4a2b827199
charls.kim <cs.kim@battleent.com>
2018-09-21 12:50:36 +0900

    AccountManager 인터페이스 추가

---
+++ contracts/interface/IAccountManager.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+contract IAccountManager {
+    function createNewAccount(string _userName, string _password, string _privateKey, address _wallet) external;
+    function login(string _userName, string _password) external view returns (string key_, bool result_);
+    function setPurchaseContentsAddress(address _contentAddress, address _buyer) external;
+    function isRegistered(string _userName) public view returns (bool isRegistered_);
+    function getUserName(address _wallet) external view returns (string userName_, bool result_);
+    event RegisterNewAccount(uint256 indexed _index, address indexed _walletAddress, string _userName);
+}
