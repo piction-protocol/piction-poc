b511935ba07dbb852f77fbf0d5c928b9e6ffdcb3
charls.kim <cs.kim@battleent.com>
2018-09-21 12:52:41 +0900

    작가명 추가로 인터페이스 수정, 이벤트 내용 수정

--- contracts/interface/IContent.sol
+++ contracts/interface/IContent.sol
@@ -8,13 +8,14 @@ contract IContent {
     function isPurchasedEpisode(uint256 _index, address _buyer) external view returns (bool isPurchased_);
     function getRecord() public view returns (string record_);
     function getWriter() public view returns (address writer_);
+    function getWriterName() public view returns (string writerName_);
     function getMarketerRate() public view returns (uint256 marketerRate_);
     function getPickCount() public view returns (uint256 pickCount_);
     function getEpisodeLength() public view returns (uint256 episodeLength_);
     function getEpisodeDetail(uint256 _index, address _buyer) external view returns (bytes record_, uint256 price_, uint256 buyCount_, bool isPurchased_);
     function getEpisodeCuts(uint256 _index) external view returns (string episodeCuts_);
     function episodePurchase(uint256 _index, address _buyer, uint256 _amount) external;
-    event ContentCreation(address indexed _sender, address indexed _writer, string _record, uint256 _marketerRate);
+    event ContentCreation(address indexed _sender, address indexed _writer, string _writerName, string _record, uint256 _marketerRate);
     event EpisodeCreation(uint256 indexed _episodeIndexId, address indexed _contentAddress, address indexed _writer, string _record, uint256 _price);
     event ChangeContent(address indexed _contentAddress, address indexed _writer, string _record, uint256 _marketerRate);
     event ChangeEpisode(uint256 indexed _episodeIndexId, address indexed _contentAddress, address indexed _writer, string _record, uint256 _price);
