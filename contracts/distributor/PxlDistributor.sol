8f4f5a4742002cd8398345a26de0f5102aed4b85
charls.kim <cs.kim@battleent.com>
2018-09-27 11:22:28 +0900

    중복된 import문 제거(openzeppelin solidity 라이브러리 최신 코드 적용)

--- contracts/distributor/PxlDistributor.sol
+++ contracts/distributor/PxlDistributor.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
-import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 import "contracts/interface/ICouncil.sol";
