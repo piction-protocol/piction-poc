73bd11502cb568d135633341dba83ac0aee4fdc1
sk.kwon <sk.kwon@battleent.com>
2018-10-08 18:13:19 +0900

    내 작품에서 이전 페이지 이동 안되는 현상 수정

--- src/components/my/contents/Index.vue
+++ src/components/my/contents/Index.vue
@@ -10,7 +10,7 @@
       </router-link>
     </b-nav>
     <hr>
-    <router-view :key="$route.path"/>
+    <router-view/>
   </div>
 </template>
 
@@ -28,8 +28,8 @@
       this.contents.forEach((content, i) => {
         content.id = contents.writerContentsAddress_[i];
       });
-      if (this.contents.length > 0 && !this.$router.history.current.params.content_id) {
-        this.$router.push({name: 'show-my-content', params: {content_id: this.contents[0].id}})
+      if (this.contents.length > 0 && !this.$route.params.content_id) {
+        this.$router.replace({name: 'show-my-content', params: {content_id: this.contents[0].id}})
       }
     }
   }
