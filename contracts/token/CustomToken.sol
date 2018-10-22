c71eca0afe00a592e9e3619aa810eb8380489218
charls.kim <cs.kim@battleent.com>
2018-10-22 11:11:59 +0900

    토큰 입/출금 내역 처리를 위하여 인터페이스 내용 추가

--- contracts/token/CustomToken.sol
+++ contracts/token/CustomToken.sol
@@ -2,5 +2,8 @@ pragma solidity ^0.4.24;
 
 contract CustomToken {
     function approveAndCall(address _to, uint256 _value, bytes _data) public returns (bool);
+    function transferFromPxl(address _from, address _to, uint256 _value, string message) public returns (bool);
+    function transferPxl(address _to, uint256 _value, string message) public returns (bool);
     event ApproveAndCall(address indexed _from, address indexed _to, uint256 _value, bytes _data);
+    event PxlTransfer(address indexed _from, address indexed _to, uint256 value, string message);
 }
