2f86b98816ba1631e0d746b47829d9fb5e3c3a49
sk.kwon <sk.kwon@battleent.com>
2018-10-22 19:31:28 +0900

    모집금액 조회 인덱스 오류 수정

--- contracts/api/ApiFund.sol
+++ contracts/api/ApiFund.sol
@@ -134,6 +134,7 @@ contract ApiFund is ValidValue {
         external
         view
         returns (
+            address content_,
             uint256 startTime_,
             uint256 endTime_,
             uint256[] memory limit_,
@@ -155,7 +156,7 @@ contract ApiFund is ValidValue {
         fundRise_ = new uint256[](_funds.length);
 
         for(uint i = 0; i < _funds.length; i++) {
-            (,,,fundRise_[i],,,,,) = IFund(_funds[i]).getFundInfo();
+            (,,,,fundRise_[i],,,,,) = IFund(_funds[i]).getFundInfo();
         }
     }
 
