c001486a5252ad2f48e92e116afe9b133f612a2d
sk.kwon <sk.kwon@battleent.com>
2018-10-22 12:24:23 +0900

    펀드 모델 생성

---
+++ src/models/Fund.js
@@ -0,0 +1,36 @@
+import Comic from '@models/Comic'
+
+export default class Fund {
+  constructor(address,
+              startTime,
+              endTime,
+              rise = 0,
+              maxcap = 0,
+              softcap = 0,
+              min = 0,
+              max = 0,
+              poolSize = 3,
+              interval = 1,
+              firstDistributionTime,
+              distributionRate = 0.01,
+              detail) {
+    this.address = address ? address.toLowerCase() : '';
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.rise = Number(rise);
+    this.maxcap = Number(maxcap);
+    this.softcap = Number(softcap);
+    this.min = Number(min);
+    this.max = Number(max);
+    this.poolSize = Number(poolSize);
+    this.interval = Number(interval);
+    this.firstDistributionTime = firstDistributionTime;
+    this.distributionRate = Number(distributionRate);
+    this.detail = detail;
+    this.comic = new Comic();
+  }
+
+  setComic(comic) {
+    this.comic = comic;
+  }
+}
