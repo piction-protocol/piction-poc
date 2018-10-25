49ed1b525f4f7183069c29a34ef3d863a3abec88
charls.kim <cs.kim@battleent.com>
2018-10-25 10:24:55 +0900

    토큰 전송 실패 버그 수정, Custom transfer 누락 된 곳 추가

--- contracts/token/PXL.sol
+++ contracts/token/PXL.sol
@@ -64,7 +64,7 @@ contract PXL is ERC20, CustomToken, ExtendsOwnable {
     }
 
     function transferFromPxl(address _from, address _to, uint256 _value, string message) public returns (bool) {
-        require(this.transferFrom(_from, _to, _value), "transfer from failed");
+        require(transferFrom(_from, _to, _value), "transfer from failed");
         emit PxlTransfer(_from, _to, _value, message);
         return true;
     }
@@ -83,7 +83,7 @@ contract PXL is ERC20, CustomToken, ExtendsOwnable {
     }
 
     function transferPxl(address _to, uint256 _value, string message) public returns (bool) {
-        require(this.transfer(_to, _value), "transfer failed");
+        require(transfer(_to, _value), "transfer failed");
         emit PxlTransfer(msg.sender, _to, _value, message);
         return true;
     }
