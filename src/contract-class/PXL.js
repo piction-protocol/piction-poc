acbb2d1ec0f42ccc90fc0c31a397ece2c61bada3
charls.kim <cs.kim@battleent.com>
2018-10-29 15:33:16 +0900

    PXL  전송 내역 이벤트 목록 조회기능 추가

--- src/contract-class/PXL.js
+++ src/contract-class/PXL.js
@@ -1,4 +1,5 @@
 import {abi} from '@contract-build-source/PXL'
+import Web3Utils from '@utils/Web3Utils'
 
 class PXL {
   constructor(address, from, gas) {
@@ -21,6 +22,42 @@ class PXL {
 
   approveAndCall(to, value, data) {
     return this._contract.methods.approveAndCall(to, value, data ? data : '0x00').send()
+  }
+
+  async getPxlTransferEvents() {
+    const fromFilter = {};
+    const toFilter = {};
+    fromFilter._from = this._contract.options.from;
+    toFilter._to = this._contract.options.from;
+
+    const transferEvents = [];
+    let fromEvents = await this._contract.getPastEvents('PxlTransfer', {filter: fromFilter, fromBlock: 0, toBlock: 'latest'});
+    let toEvents = await this._contract.getPastEvents('PxlTransfer', {filter: toFilter, fromBlock: 0, toBlock: 'latest'});
+
+    fromEvents.forEach(async event =>{
+      event = Web3Utils.prettyJSON(event.returnValues);
+      event.isPlus = false;
+      transferEvents.push(event);
+    });
+
+    toEvents.forEach(async event =>{
+      event = Web3Utils.prettyJSON(event.returnValues);
+      event.isPlus = true;
+      transferEvents.push(event);
+    });
+
+    transferEvents.sort(function(a,b) {
+      if (a.time > b.time) {
+        return 1;
+      }
+      if (a.time < b.time) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+
+    return transferEvents;
   }
 }
 
